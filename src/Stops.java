ac6ef49bfbd0da85f02ee30414040926c9069436
WannaS1eep <75248058+WannaS1eep@users.noreply.github.com>
2021-06-12 11:14:27 +0200

    Add files via upload

---
+++ src/Stops.java
@@ -0,0 +1,12 @@
+public class Stops {
+    public String stopId;
+    public String stopName;
+    public double stopLat;
+    public double stopLon;
+    Stops(String stopId, String stopName,Double stopLat,Double stopLon){
+        this.stopId = stopId;
+        this.stopName = stopName;
+        this.stopLat = stopLat;
+        this.stopLon = stopLon;
+    }
+}
