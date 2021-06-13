29f806f10f34088bda49edad96ea84294bca50cc
WannaS1eep <maj9801@outlook.com>
2021-06-13 12:10:34 +0200

    Finished clustering part. In this class, you can use deleteEdge() to delete one edge with max betweenness or user reachMaxClustering(int n) to let your graph reach n clusters. :)

---
+++ src/pathAlgo/Clustering.java
@@ -0,0 +1,183 @@
+package pathAlgo;
+
+import graph.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Clustering{
+    private Graph g;
+
+    //EdgeName : ("Src,dst"), double value of betweenness
+    HashMap<String, Double> betweenness = new HashMap<>();
+
+    double shortestDistance = Double.MAX_VALUE;
+    ArrayList<String> shortestPath = new ArrayList<>();
+
+    public Clustering(Graph g){
+        this.g = g;
+    }
+
+    // make the graph reach the requested maximum number of clusters.
+    public void reachMaxClusters(int n){
+        if(CountClusters() >= n){
+            System.out.println("The graph already reached the requested maximum number of clusters");
+            g.printGraph();
+            return;
+        }
+        while(CountClusters()<n){
+            deleteEdge();
+        }
+        System.out.println("now the number of cluster is " + n);
+        g.printGraph();
+    }
+
+
+    private int CountClusters(){
+        return BFS.cc(g);
+    }
+
+
+    public void deleteEdge(){
+        //计算每条边的betweenness
+        calAllBetweenness();
+        //遍历得到最大的betweenness
+        String maxEdge = "";
+        double maxbetweenness = -1;
+        for(String name : betweenness.keySet()){
+            if(betweenness.get(name) > maxbetweenness){
+                maxbetweenness = betweenness.get(name);
+                maxEdge = name;
+            }
+        }
+        System.out.println("Deleted the edge " + maxEdge);
+        //delete all this edges in the graph.myGraph
+        String[] maxStops = maxEdge.split(",");
+        for(int i=0; i<g.getMyGraph().get(maxStops[0]).size();i++){
+            if(g.getMyGraph().get(maxStops[0]).get(i).getDst().getstopName().equals(maxStops[1])){
+                g.getMyGraph().get(maxStops[0]).remove(i);
+            }
+        }
+        for(int i=0; i<g.getMyGraph().get(maxStops[1]).size();i++){
+            if(g.getMyGraph().get(maxStops[1]).get(i).getDst().getstopName().equals(maxStops[0])){
+                g.getMyGraph().get(maxStops[1]).remove(i);
+            }
+        }
+
+    }
+
+    //计算所有边的betweenness
+    private void calAllBetweenness(){
+
+        //初始化每条边对应的HashMap
+        betweenness = new HashMap<>();
+        for(List<Edges> eList : g.getMyGraph().values()){
+            for(Edges e : eList){
+                if(!betweenness.containsKey(e.getDst().getstopName()+","+e.getSrc().getstopName())){
+                    betweenness.put(e.getSrc().getstopName()+","+e.getDst().getstopName(), 0.0);
+                }
+            }
+        }
+        for(Stops from : g.getMyStops().values()){
+            for(Stops to : g.getMyStops().values()){
+                if(!from.getstopName().equals(to.getstopName())){
+//                    System.out.println(from.getstopName()+"  to  "+to.getstopName());
+                    getAllSP(from.getstopName(),to.getstopName());
+                    //如果存在最短路径
+                    if(shortestDistance != Double.MAX_VALUE){
+                        double sum = 1.0/shortestPath.size();
+                        for (String path :shortestPath){
+                            String[] nodes = path.split(",");
+                            for(int i=0;i< nodes.length-1;i++){
+                                if(betweenness.containsKey(nodes[i]+","+nodes[i+1])){
+                                    betweenness.put(nodes[i]+","+nodes[i+1], betweenness.get(nodes[i]+","+nodes[i+1])+sum);
+                                }else{
+                                    betweenness.put(nodes[i+1]+","+nodes[i], betweenness.get(nodes[i+1]+","+nodes[i])+sum);
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+
+    private void getAllSP(String src, String dst){
+        shortestDistance = Double.MAX_VALUE;
+        shortestPath = new ArrayList<>();
+        Stops srcStops = g.getMyStops().get(src);
+        Stops dstStops = g.getMyStops().get(dst);
+        Nodes n = new Nodes();
+        n.name = "begin";
+        n.front = null;
+        n.distance = 0;
+        getAllPath(1, n, srcStops, dstStops);
+    }
+
+
+    //得到所有src到dst的路径和路径长度
+    private void getAllPath(double distanceBefore,Nodes lastNode, Stops src, Stops dst){
+
+        Nodes n = lastNode;
+        while (n.distance != 0){
+            n = n.front;
+            //如果前面的点里已经有这个src了 就退出递归
+            if(n.name.equals(src.getstopName())){
+                return;
+            }
+        }
+        Nodes srcNode = new Nodes();
+        srcNode.name = src.getstopName();
+        if(!lastNode.name.equals("begin")){
+            for(Edges e : g.getMyGraph().get(lastNode.name)){
+                if (e.getDst().getstopName().equals(src.getstopName())){
+                    srcNode.distance = distanceBefore + e.getdistance();
+                }
+            }
+        }else {
+            srcNode.distance = 0;
+        }
+
+        lastNode.next.add(srcNode);
+        srcNode.front = lastNode;
+        //如果是终点，退出递归
+        if(src.getstopName().equals(dst.getstopName())){
+            //如果是最短路径的话
+            if(srcNode.distance <= shortestDistance){
+                String onePath = "";
+                Nodes m = srcNode;
+                //倒着的一条路线
+                while (!m.name.equals("begin")){
+                    onePath = onePath + m.name +",";
+                    m = m.front;
+                }
+                if(srcNode.distance < shortestDistance){
+                    shortestPath = new ArrayList<>();
+                    shortestDistance = srcNode.distance;
+                }
+                shortestPath.add(onePath);
+            }
+            return;
+        }
+        for (Edges e : g.getMyGraph().get(src.getstopName())){
+            getAllPath(srcNode.distance, srcNode, e.getDst(), dst);
+        }
+
+    }
+
+    //链表
+    private class Nodes {
+        ArrayList<Nodes> next = new ArrayList<>();
+        Nodes front;
+        String name;
+        double distance;
+        Nodes(){
+
+        }
+
+    }
+
+}
