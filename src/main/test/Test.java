55b4c8b64581e32918cbd14ae484139c6c93e843
WannaS1eep <maj9801@outlook.com>
2021-06-16 18:07:42 +0200

    数据可视化， 调用DrawGraph 需要传Graph和最短路径点的String的List

--- src/main/test/Test.java
+++ src/main/test/Test.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 
 import graph.*;
 import pathAlgo.*;
+import utils.*;
 
 public class Test {
 
@@ -13,37 +14,41 @@ public class Test {
 		
 		Graph g = new Graph();
 		
-//		readStop(g);
-//		readRoute(g);
+		readStop(g);
+		readRoute(g);
 
-		g.addStop("1,a,0,0");
-		g.addStop("2,b,0,0");
-		g.addStop("3,c,0,0");
-		g.addStop("4,d,0,0");
-		g.addStop("5,e,0,0");
-		g.addStop("6,f,0,0");
-		g.addStop("7,g,0,0");
-		g.addStop("8,h,0,0");
-		g.addStop("9,i,0,0");
+		System.out.println(g.minLat+","+g.maxLat);
+//		g.addStop("1,a,0,0");
+//		g.addStop("2,b,1,0");
+//		g.addStop("3,c,0,1");
+//		g.addStop("4,d,1,1");
+//		g.addStop("5,e,2,5");
+//		g.addStop("6,f,3,3");
+//		g.addStop("7,g,4,3");
+//		g.addStop("8,h,5,5");
+//		g.addStop("9,i,6,6");
+//
+//		g.addRoute("r1,1,2,3,4,1");
+//		g.addRoute("r2,2,4");
+//		g.addRoute("r3,1,3");
+//		g.addRoute("r4,4,5,6,7,8,5");
+//		g.addRoute("r5,6,8");
+//		g.addRoute("r1,1,3,4");
+//		g.addRoute("r2,2,3");
 
-		g.addRoute("r1,1,2,3,4,1");
-		g.addRoute("r2,2,4");
-		g.addRoute("r3,1,3");
-		g.addRoute("r4,4,5,6,7,8,5");
-		g.addRoute("r5,6,8");
-
-
-		Clustering cluster = new Clustering(g);
-		cluster.reachMaxClusters(13);
+//		Clustering cluster = new Clustering(g);
+//		cluster.reachMaxClusters(13);
 //		cluster.deleteEdge();
 
 //		BFS b = new BFS();
 //		b.bfs(g, "a");
 //		System.out.println(b.getSP("i"));
 //
-//		Dijkstra d = new Dijkstra();
-//		d.DijkstraSP(g, "a");
-//		System.out.println(d.getSP("i"));
+		Dijkstra d = new Dijkstra();
+		d.DijkstraSP(g, "Laurentina/Torretta Vallerano");
+		System.out.println(d.getSP("Tuscolana/Appio Claudio"));
+		DrawGraph dw = new DrawGraph(g, d.getSP("Tuscolana/Appio Claudio"));
+
 	}
 	
 	public static void readStop(Graph g ) {
